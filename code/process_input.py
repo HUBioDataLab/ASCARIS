2c267802653b2f263d0dc3cf04450288d595ba0b
fatmacankara <fatmacankaraa@gmail.com>
2024-01-11 16:02:38 +0300

    Update process_input.py

--- code/process_input.py
+++ code/process_input.py
@@ -1,25 +1,37 @@
 import pandas as pd
-
+import streamlit as st
 def clean_data(input_set):
     data = pd.DataFrame()
     try:
         if ',' in input_set:
             input_set = [i.strip() for i in input_set.split(',')]
+            initial_len = len(input_set)
+            
             for i in input_set:
                 data = data.append(pd.Series([j.strip() for j in i.split('-')]), ignore_index=True)
             data.columns = ['uniprotID', 'wt', 'pos', 'mut']
+            data = data[((~data.uniprotID.isna()) & (~data.wt.isna()) & (~data.pos.isna()) & (~data.mut.isna()))]
+            if initial_len != len(data):
+                st.write(f'{initial_len- len(data)} of {initial_len} datapoints is omitted. Check your input.')
         elif '\t' in input_set:
             input_set = [i.strip() for i in input_set.split('\t')]
+            initial_len = len(input_set)
+            
             for i in input_set:
                 data = data.append(pd.Series([j.strip() for j in i.split('-')]), ignore_index=True)
             data.columns = ['uniprotID', 'wt', 'pos', 'mut']
-
+            data = data[((~data.uniprotID.isna()) & (~data.wt.isna()) & (~data.pos.isna()) & (~data.mut.isna()))]
+            if initial_len != len(data):
+                st.write(f'{initial_len- len(data)} of {initial_len} datapoints is omitted. Check your input.')
         elif '-' in input_set:
             data = data.append(pd.Series([j.strip() for j in input_set.split('-')]), ignore_index=True)
             data.columns = ['uniprotID', 'wt', 'pos', 'mut']
 
         elif '.txt' in input_set:
             data = pd.read_csv(input_set, sep='\t', names=['uniprotID', 'wt', 'pos', 'mut'])
+
+        else:
+            data =  pd.DataFrame(columns = ['uniprotID', 'wt', 'pos', 'mut'])
         data = data[['uniprotID', 'wt', 'pos', 'mut']]
 
         # Exclude termination codons, synonymous mutations and any non-standard residues such as Sec, 4 or 6.
@@ -33,8 +45,8 @@ def clean_data(input_set):
             data.at[i, 'datapoint'] = data.at[i, 'uniprotID'] + data.at[i, 'wt'] + str(data.at[i, 'pos']) + data.at[i, 'mut']
 
         data = data.astype(str)
+        
         return data
-    except:
-        ValueError
-        print('Please check the input format.')
-
+    except ValueError:
+        st.write('Your input is in the wrong format. Please see the example.')
+        return pd.DataFrame()
