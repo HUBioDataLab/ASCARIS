d7e1da4f35eb24e2f3568ec8786272089bee278b
fatmacankara <fatmacankaraa@gmail.com>
2024-01-11 16:03:35 +0300

    Update standard.py

--- code/standard.py
+++ code/standard.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def standardize(df, get_columns):
     cols_to_change = ['sasa', 'domaindistance3D', 'disulfide', 'intMet', 'intramembrane',
                       'naturalVariant', 'dnaBinding', 'activeSite', 'nucleotideBinding',
@@ -11,3 +13,24 @@ def standardize(df, get_columns):
     df = df[get_columns.columns]
 
     return df
+
+
+def finalTouch(data):
+    for i in data.index:
+        if '*' in data.at[i, 'sasa']:
+            data.at[i, 'sasa'] = data.at[i, 'sasa'].split('*')[0]
+    data.sasa = data.sasa.replace({'N/A': np.NaN})
+    data.replace({'   N/A': np.NaN}, inplace=True)
+    data.replace({'None': np.NaN, '':np.NaN}, inplace=True)
+    data.sasa = data.sasa.astype(float)
+    data = data.astype(str)
+    for i in data.index:
+        if float(data.at[i, 'sasa']) < 5:
+            data.at[i, 'trsh4'] = 'core'
+        elif float(data.at[i, 'sasa']) >= 5:
+            data.at[i, 'trsh4'] = 'surface'
+        elif data.at[i, 'sasa'] == 'nan':
+            data.at[i, 'trsh4'] = 'nan'
+
+
+    return data
